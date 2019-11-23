a10c717e921df72288966984495cbaf65cfd3c2d
Martin R <23411718+RonquilloAeon@users.noreply.github.com>
2019-11-23 08:28:17 -0600

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='migri',
-    version='0.1.2',
+    version='0.1.3',
     install_requires=[
         'aiofiles==0.4.*',
         'asyncpg==0.18.*',
