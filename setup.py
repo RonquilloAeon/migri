d412b11a40b38873096511e76105edb55633635a
Martin R <23411718+RonquilloAeon@users.noreply.github.com>
2020-06-06 14:53:06 -0500

    Add long description and update classifiers in setup.py

--- setup.py
+++ setup.py
@@ -1,29 +1,35 @@
 from setuptools import setup, find_packages
 
 
+with open("README.md", "r", encoding="utf-8") as f:
+    readme = f.read()
+
 setup(
-    name='migri',
-    version='0.2.0',
+    name="migri",
+    version="0.2.0",
+    long_description=readme,
+    long_description_content_type="text/markdown",
     install_requires=[
-        'aiofiles==0.4.*',
-        'asyncpg>=0.18.*',
-        'click==7.*',
-        'sqlparse==0.3.*',
+        "aiofiles==0.4.*",
+        "asyncpg>=0.18.*",
+        "click==7.*",
+        "sqlparse==0.3.*",
     ],
     entry_points={
-        'console_scripts': [
-            'migri = migri.main:main'
+        "console_scripts": [
+            "migri = migri.main:main"
         ]
     },
     packages=find_packages(),
     include_package_data=True,
-    python_requires='>=3.7',
+    python_requires=">=3.7",
     classifiers=[
-        'Development Status :: 1 - Planning',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Database',
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Database",
     ]
 )
