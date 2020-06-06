61bb8df2cf0206e316164a266ddcf7838637b6a6
Martin R <23411718+RonquilloAeon@users.noreply.github.com>
2020-06-06 10:58:33 -0500

    Reformat test code for consistency and add test for dry run

--- test/conftest.py
+++ test/conftest.py
@@ -7,23 +7,21 @@ from typing import Generator
 from migri import run_initialization
 
 TEST_DB_CREDS = {
-    'db_user': 'migrator',
-    'db_pass': 'passpass',
-    'db_name': 'migritestdb',
-    'db_host': 'localhost',
-    'db_port': '5432',
+    "db_user": "migrator",
+    "db_pass": "passpass",
+    "db_name": "migritestdb",
+    "db_host": "localhost",
+    "db_port": "5432",
 }
 
 
 @asynccontextmanager
 async def db_conn(
-    db_user: str,
-    db_pass: str,
-    db_name: str,
-    db_host: str,
-    db_port: str
+    db_user: str, db_pass: str, db_name: str, db_host: str, db_port: str
 ) -> Generator[asyncpg.Connection, None, None]:
-    conn = await asyncpg.connect(f'postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}')
+    conn = await asyncpg.connect(
+        f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}"
+    )
 
     try:
         yield conn
@@ -43,14 +41,14 @@ async def conn() -> Generator[asyncpg.connection.Connection, None, None]:
 
 
 async def execute(command: str, dbname: str) -> str:
-    async with db_conn(**{**TEST_DB_CREDS, 'db_name': dbname}) as conn:
+    async with db_conn(**{**TEST_DB_CREDS, "db_name": dbname}) as conn:
         return await conn.execute(command)
 
 
 def pytest_configure(config):
-    asyncio.run(execute('CREATE DATABASE migritestdb', 'postgres'))
+    asyncio.run(execute("CREATE DATABASE migritestdb", "postgres"))
     asyncio.run(run_initialization(**TEST_DB_CREDS))
 
 
 def pytest_unconfigure(config):
-    asyncio.run(execute('DROP DATABASE migritestdb', 'postgres'))
+    asyncio.run(execute("DROP DATABASE migritestdb", "postgres"))
--- test/migrations_a/0002_add_initial_data.py
+++ test/migrations_a/0002_add_initial_data.py
@@ -1,14 +1,14 @@
 async def migrate(conn) -> bool:
-    await conn.execute('INSERT INTO account (name) VALUES ($1)', 'My Account')
-    account = await conn.fetchrow('SELECT * FROM account WHERE name = $1', 'My Account')
+    await conn.execute("INSERT INTO account (name) VALUES ($1)", "My Account")
+    account = await conn.fetchrow("SELECT * FROM account WHERE name = $1", "My Account")
 
     await conn.execute(
-        'INSERT INTO app_user (account_id, first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5)',
-        account['id'],
-        'M',
-        'Test',
-        'test@example.com',
-        '078bbb4bf0f7117fb131ec45f15b5b87'
+        "INSERT INTO app_user (account_id, first_name, last_name, email, password) VALUES ($1, $2, $3, $4, $5)",
+        account["id"],
+        "M",
+        "Test",
+        "test@example.com",
+        "078bbb4bf0f7117fb131ec45f15b5b87",
     )
 
     return True
